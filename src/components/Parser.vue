c6b2a70f8d8f22420b4a600053189b88222bc464
m-nick <sunty1@mail.ru>
2019-11-08 13:30:08 +0300

    Add get data from dragged file

--- src/components/Parser.vue
+++ src/components/Parser.vue
@@ -2,34 +2,43 @@
   <div class="parser">
     <div class="parser__from">
       <div class="parser__title">From Xml</div>
-      <div class="parser__body"><textarea v-model="textSource"></textarea></div>
+      <div ref="dragClassEl" class="parser__body">
+        <textarea ref="dragArea" v-model="textSource"></textarea>
+      </div>
     </div>
     <button @click="parse" class="parser__button">> PARSE ></button>
     <div class="parser__to">
       <div class="parser__title">To Json</div>
-      <div class="parser__body"><textarea v-model="textResult"></textarea></div>
+      <div class="parser__body">
+        <textarea v-model="textResult"></textarea>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Xml from '@/js/xml/xml.js'
+import dragModule from '@/modules/drag-n-drop.js'
 
 export default {
   name: 'Parser',
   data(){
     return {
-      // textSource:`<a href="https://www.some-site.com">SomeSite</a>\n<a href=https://www.some-site.com>SomeSite</a>\n<a href="https://www.some-site.com"></a>\n<a href=https://www.some-site.com></a>\n<a></a>\n<a/>`,
-      textSource:`a;l;k;s;j;d;l;k;a;j;s;d;<a href="https://www.some-site.com">SomeSite</a>alksdjhfl;akjsd;hf`,
+      // textSource:`<a href="https://www.some-site.com">SomeSite</a><a href=https://www.some-site.com>SomeSite</a><a href="https://www.some-site.com"></a><a href=https://www.some-site.com></a><a></a><a/>`,
+      textSource:``,
       textResult: '',
     }
   },
+  mounted(){
+    this.addDragEvents()
+  },
   methods: {
+    addDragEvents(){
+      dragModule.addDragEvents.call(this, dragModule)
+    },
     parse(){
-      const parsedXml = new Xml(this.textSource)
-      console.log(parsedXml)
-      // this.textResult = JSON.stringify(xmlToJson.parse(this.textSource))
-    }
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(this.textSource, "text/xml");
+    },
   }
 };
 </script>
---
+++ src/modules/drag-n-drop.js
@@ -0,0 +1,90 @@
+const thisModule = {
+  addProxyDrag(fn){
+    const handler = {
+      apply: (target, thisArg, args) => {
+        if(args && args[0] && args[0].dataTransfer &&
+                args[0].dataTransfer &&
+                args[0].dataTransfer.types){
+          for (var i = 0; i < args[0].dataTransfer.types.length; i++) {
+              if (args[0].dataTransfer.types[i] == "Files") {
+                return target.apply(thisArg, args)
+              }
+          }
+        }
+      }
+    }
+    return new Proxy(fn, handler)
+  },
+
+  addDragEvents(thisModule){
+    const dragArea = this.$refs.dragArea
+    window.addEventListener('dragover', thisModule.addProxyDrag(thisModule.handleWindowDragOver.bind(this)))
+    window.addEventListener('drop', thisModule.addProxyDrag(thisModule.handleWindowDrop.bind(this)))
+    dragArea.addEventListener('dragenter', thisModule.addProxyDrag(thisModule.handleAreaDragEnter.bind(this)))
+    dragArea.addEventListener('dragover', thisModule.addProxyDrag(thisModule.handleAreaDragOver.bind(this)))
+    dragArea.addEventListener('dragleave', thisModule.addProxyDrag(thisModule.handleAreaDragLeave.bind(this)))
+    dragArea.addEventListener('drop', thisModule.addProxyDrag(thisModule.handleAreaDrop.bind(this)))
+  },
+
+  async handleAreaDragEnter(e){
+    e.preventDefault()
+    e.stopPropagation()
+    const dragClassEl = this.$refs.dragClassEl
+    if(dragClassEl){
+      dragClassEl.classList.add('parser__body--drag-area', 'parser__body--drag-over')
+      dragClassEl.classList.remove('parser__body--drag-out')
+    }
+  },
+
+  async handleAreaDragOver(e){
+    e.preventDefault()
+    e.stopPropagation()
+    const dragClassEl = this.$refs.dragClassEl
+    if(dragClassEl){
+      dragClassEl.classList.add('parser__body--drag-area', 'parser__body--drag-over')
+      dragClassEl.classList.remove('parser__body--drag-out')
+    }
+  },
+
+  handleAreaDragLeave(e){
+    e.preventDefault()
+    e.stopPropagation()
+    const dragClassEl = this.$refs.dragClassEl
+    if(dragClassEl){
+      dragClassEl.classList.add('parser__body--drag-area', 'parser__body--drag-out')
+      dragClassEl.classList.remove('parser__body--drag-over')
+    }
+  },
+
+  async handleAreaDrop(e){
+    e.preventDefault()
+    e.stopPropagation()
+    const dragClassEl = this.$refs.dragClassEl
+    const file = e.dataTransfer.files[0]
+    if(file){
+      this.textSource = await file.text('xml')
+    }
+    if(dragClassEl){
+      dragClassEl.classList.remove('parser__body--drag-over', 'parser__body--drag-area', 'parser__body--drag-out')
+    }
+  },
+
+  handleWindowDragOver(e){
+    e.preventDefault()
+    const dragClassEl = this.$refs.dragClassEl
+    if(dragClassEl){
+      dragClassEl.classList.add('parser__body--drag-area', 'parser__body--drag-out')
+      dragClassEl.classList.remove('parser__body--drag-over')
+    }
+  },
+
+  handleWindowDrop(e){
+    e.preventDefault()
+    const dragClassEl = this.$refs.dragClassEl
+    if(dragClassEl){
+      dragClassEl.classList.remove('parser__body--drag-over', 'parser__body--drag-area', 'parser__body--drag-out')
+    }
+  },
+}
+
+export default thisModule
